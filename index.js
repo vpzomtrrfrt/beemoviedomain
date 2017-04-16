efec73138ce18414047467df62b82a1b796dc541
Colin Reeder <vpzomtrrfrt@gmail.com>
2017-04-15 20:32:56 -0600

    Lowercase, extra dot

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var OFFSET1 = 93;
 var OFFSET2 = 54;
 var OFFSET3 = 2;
 
+var EXTRA_DOT = 57;
+
+
 var fs = require('fs');
 
 var content = fs.readFileSync("script.txt");
@@ -11,7 +14,7 @@ for(var i = 0; i < content.length; i++) {
 	var code = content.charCodeAt(i);
 	if(code >= 65 && code <= 90) {
 		// A-Z
-		result += content[i];
+		result += content[i].toLowerCase();
 	}
 	else if(code >= 97 && code <= 122) {
 		result += content[i];
@@ -19,6 +22,7 @@ for(var i = 0; i < content.length; i++) {
 }
 
 var subdomain = result.substring(0, OFFSET1);
+subdomain = subdomain.substring(0, EXTRA_DOT)+"."+subdomain.substring(EXTRA_DOT);
 var domain = result.substring(OFFSET1, OFFSET1+OFFSET2);
 var tld = result.substring(OFFSET1+OFFSET2, OFFSET1+OFFSET2+OFFSET3);
 
@@ -27,6 +31,7 @@ var path = result.substring(OFFSET1+OFFSET2+OFFSET3);
 var host = subdomain+"."+domain+"."+tld;
 //var host = "localhost:2333";
 
+console.log(host);
 var http = require('http');
 http.createServer(function(req, res) {
 	console.log(req.url);
